0a692cf4b137828437117ef09f1b63d4df60383b
marcyb5st <steiner.marcello@gmail.com>
2014-01-11 19:44:58 +0100

    test

--- app/controllers/MainController.java
+++ app/controllers/MainController.java
@@ -4,24 +4,26 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 public class MainController extends Controller {
+
+
     
-    public static Result index() {
-        return ok(views.html.index.render("Hello from Java"));
+    public static Result index(String color) {
+        return ok(views.html.index.render(color));
     }
 
-    public static Result missions(){
-    	return ok(views.html.missions.render());
+    public static Result missions(String color){
+    	return ok(views.html.missions.render(color));
     }
     
-    public static Result map(){
-    	return ok(views.html.map.render());
+    public static Result map(String color){
+    	return ok(views.html.map.render(color));
     }
 
-    public static Result mapP2P(){
-    	return ok(views.html.mapP2P.render());
+    public static Result mapP2P(String color){
+    	return ok(views.html.mapP2P.render(color));
     }
 
-    public static Result mission1(){
-    	return ok(views.html.mission1.render());
+    public static Result mission1(String color){
+    	return ok(views.html.mission1.render(color));
     }
 }
